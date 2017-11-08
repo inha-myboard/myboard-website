e792695c679d16c395ff22f7ecc363bd10de835b
bdh92123 <bdh92123.1@gmail.com>
2017-11-08 16:19:17 +0900

    Test widget json

--- js/myboards.js
+++ js/myboards.js
@@ -1,20 +1,88 @@
+// 편집모드 활성화
+function enableEdit() {
+	gridstack.movable(gridstack.container.children('.' + gridstack.opts.itemClass), true);
+	gridstack.resizable(gridstack.container.children('.' + gridstack.opts.itemClass), true);
+	$(".nav-edit", "ul.nav").show();
+	$(".nav-normal", "ul.nav").hide();
+}
 
+// 편집모드 비활성화
+function disableEdit() {
+	gridstack.disable();
+	$(".nav-edit", "ul.nav").hide();
+	$(".nav-normal", "ul.nav").show();
+}
+
+// 편집중인 보드 전체저장
+function saveBoard() {
+
+}
+
+var testProps = {
+   "fields":[{
+         "api_path":"rank",
+         "type":"text",
+         "caption": "순위",
+         "width":"20%",
+         "alignment":"right",
+         "font_size":12,
+         "font_color":"0x000000",
+         "bg_color":"0xffffff"
+      }, {
+         "api_path":"keyword",
+         "type":"text",
+         "caption": "검색어",
+         "width":"80%",
+         "alignment":"middle",
+         "font_size":12,
+         "font_color":"0x000000",
+         "bg_color":"0xffffff"
+      }
+   ]
+}
+
+var testData = [{
+   "rank": {
+   	  "type": "text",
+      "text": "1"
+   },
+   "keyword": {
+   	  "type": "text",
+      "text": "ASD",
+      "href": "http://www.coinone.com"
+   },
+   "img": {
+   	  "type": "img",
+      "src": "img/shop-img4.jpg",
+      "href": "http://www.google.com"
+   }
+}, {
+   "rank": {
+   	  "type": "text",
+      "text": "2"
+   },
+   "keyword": {
+   	  "type": "text",
+      "text": "ASD",
+      "href": "http://www.naver.com"
+   },
+   "img": {
+   	  "type": "img",
+      "src": "img/shop-img3.jpg"
+   }
+}];
 
 $(document).on("ready", function(){
 	$('.grid-stack').gridstack();
 	var gridstack = $('.grid-stack').data("gridstack");
 	$("#editButton").on("click", function(){
-		gridstack.movable(gridstack.container.children('.' + gridstack.opts.itemClass), true);
-		gridstack.resizable(gridstack.container.children('.' + gridstack.opts.itemClass), true);
-		$(".nav-edit", "ul.nav").show();
-		$(".nav-normal", "ul.nav").hide();
+		enableEdit();	
 	});
 	$("#exitButton").on("click", function(){
-		gridstack.disable();
-		$(".nav-edit", "ul.nav").hide();
-		$(".nav-normal", "ul.nav").show();
+		disableEdit();
 	});
 	$("#saveButton").on("click", function(){
-		// Save Action
+		saveBoard();
+		disableEdit();
 	});
 });
