2b311d5839fa270809c701ebc84e61af90bdbe2e
wlsdnrqo11 <wlsdnrqo11@gmail.com>
2017-11-10 20:23:23 +0900

    Edit test data

--- js/myboards.js
+++ js/myboards.js
@@ -23,11 +23,11 @@ function saveBoard() {
 // 테스트용 Single Widget
 var testSingleWidget = {
     "id": 1,
+    "type": "single",
     "api_name": "naver",
     "api_user":"admin",
-    "caption": "컴퓨터학과 공지사항",
+    "caption": "인기검색어",
     "description": "desc",
-    "type": "single",
     "mapping_json": {
         "headers": [{
             "caption": "순위",
@@ -54,6 +54,82 @@ var testSingleWidget = {
                 }
             }
         ]
+    },
+    "props_json": {
+        "repeats": {
+           "start_index":0,
+           "repeat_cnt":10,
+           "direction":"down",
+           "paging":{
+                "type":"newline",
+                "max_item":"5"
+            }
+        },
+        "bound": {
+            "x": 0,
+            "y": 0,
+            "width": 9,
+            "height": 4
+        }
+    },
+    "created_time": "2017-02-01 22:11:11"
+};
+
+var testSingleWidget2 = {
+    "id": 2,
+    "type": "single",
+    "api_name": "cseboard",
+    "api_user":"admin",
+    "caption": "컴퓨터학과 공지사항",
+    "description": "desc",
+    "mapping_json": {
+        "headers": [{
+            "caption": "번호",
+            "width":"10%"
+         },{
+            "caption": "제목",
+            "width":"60%"
+         },{
+            "caption": "작성자",
+            "width":"10%"
+         },{
+            "caption": "작성일",
+            "width":"20%"
+         }],
+        "fields":[{
+             "api_path":"index",
+             "style": {
+                 "text-align":"left",
+                 "font-size":"12px",
+                 "color":"#000000",
+                 "background-color":"#ffffff"
+             },
+             "class": ["a", "b"]
+         }, {
+             "api_path":"title",
+             "style": {
+                 "text-align":"left",
+                 "font-size":"12px",
+                 "color":"#000000",
+                 "background-color":"#ffffff"
+              }
+         }, {
+             "api_path":"writer",
+             "style": {
+                 "text-align":"left",
+                 "font-size":"12px",
+                 "color":"#000000",
+                 "background-color":"#ffffff"
+              }
+         }, {
+             "api_path":"date",
+             "style": {
+                 "text-align":"left",
+                 "font-size":"12px",
+                 "color":"#000000",
+                 "background-color":"#ffffff"
+              }
+         }]
     },
     "props_json": {
         "repeats": {
@@ -133,14 +209,15 @@ var testCompositeWidget = {
     "created_time": "2017-02-01 22:11:11"
 };
 
-var testData = [{
+var testData = [
+[{
    "rank": {
       "type": "text",
       "text": "1"
    },
    "keyword": {
       "type": "text",
-      "text": "ASD",
+      "text": "coinone",
       "href": "http://www.coinone.com"
    },
    "img": {
@@ -155,19 +232,110 @@ var testData = [{
    },
    "keyword": {
       "type": "text",
-      "text": "ASD",
+      "text": "naver",
       "href": "http://www.naver.com"
    },
    "img": {
       "type": "img",
       "src": "img/shop-img3.jpg"
    }
-}];
+}], [{
+   "index": {
+      "type": "text",
+      "text": "400"
+   }, "title" : {
+      "type": "text",
+      "text": "2018-1학기 수강신청 변경 사항 안내",
+      "href": "http://cse.inha.ac.kr/board_notice/View.aspx?Seq=23297&CateNum=0&PageNum=1&SearchField=Title&Keyword=&SortExp=&SortDir=0"
+   }, "writer": {
+      "type": "text",
+      "text": "관리자"
+   }, "date": {
+      "type": "text",
+      "text": "2017.11.06"
+   }
+}, {
+   "index": {
+      "type": "text",
+      "text": "399"
+   }, "title" : {
+      "type": "text",
+      "text": "2017학년도 2학기 인하졸업우수인재 인증제안내",
+      "href": "http://cse.inha.ac.kr/board_notice/View.aspx?Seq=23296&CateNum=0&PageNum=1&SearchField=Title&Keyword=&SortExp=&SortDir=0"
+   }, "writer": {
+      "type": "text",
+      "text": "관리자"
+   }, "date": {
+      "type": "text",
+      "text": "2017.11.06"
+   }
+}, {
+   "index": {
+      "type": "text",
+      "text": "398"
+   }, "title" : {
+      "type": "text",
+      "text": "2017학년도 동계방학 러시아 파견 프로그램 안내",
+      "href": "http://cse.inha.ac.kr/board_notice/View.aspx?Seq=23252&CateNum=0&PageNum=1&SearchField=Title&Keyword=&SortExp=&SortDir=0"
+   }, "writer": {
+      "type": "text",
+      "text": "이지아"
+   }, "date": {
+      "type": "text",
+      "text": "2017.11.01"
+   }
+}, {
+   "index": {
+      "type": "text",
+      "text": "397"
+   }, "title" : {
+      "type": "text",
+      "text": "제17회 한국대학생프로그래밍경시대회 안내",
+      "href": "http://cse.inha.ac.kr/board_notice/View.aspx?Seq=23247&CateNum=0&PageNum=1&SearchField=Title&Keyword=&SortExp=&SortDir=0"
+   }, "writer": {
+      "type": "text",
+      "text": "관리자"
+   }, "date": {
+      "type": "text",
+      "text": "2017.10.31"
+   }
+}, {
+   "index": {
+      "type": "text",
+      "text": "396"
+   }, "title" : {
+      "type": "text",
+      "text": "국제처 동계방학 단기파견교육연수 프로그램 안내",
+      "href": "http://cse.inha.ac.kr/board_notice/View.aspx?Seq=23238&CateNum=0&PageNum=1&SearchField=Title&Keyword=&SortExp=&SortDir=0"
+   }, "writer": {
+      "type": "text",
+      "text": "관리자"
+   }, "date": {
+      "type": "text",
+      "text": "2017.10.31"
+   }
+}, {
+   "index": {
+      "type": "text",
+      "text": "395"
+   }, "title" : {
+      "type": "text",
+      "text": "2017학년도 외국어성적 우수장학생 선발관련",
+      "href": "http://cse.inha.ac.kr/board_notice/View.aspx?Seq=23213&CateNum=0&PageNum=1&SearchField=Title&Keyword=&SortExp=&SortDir=0"
+   }, "writer": {
+      "type": "text",
+      "text": "관리자"
+   }, "date": {
+      "type": "text",
+      "text": "2017.10.26"
+   }
+}],
+[ /* Composite data */ ]];
 
 
 
 
-var testWidgets = [testSingleWidget, $.extend(true, {}, testSingleWidget, {
+var testWidgets = [ $.extend(true, {}, testSingleWidget, {
     "caption": "인기검색어",
     "props_json": {
         "bound": {
@@ -177,7 +345,7 @@ var testWidgets = [testSingleWidget, $.extend(true, {}, testSingleWidget, {
             "height": 7
         }
     }
-}, true), testCompositeWidget];
+}, true), testSingleWidget2, testCompositeWidget];
 
 
 // 위젯 생성
@@ -216,7 +384,7 @@ $(document).on("ready", function(){
             });
 
             for(var i = 0 ; i < testWidgets.length; i++) {
-                addWidget(testWidgets[i], testData);    
+                addWidget(testWidgets[i], testData[i]);    
             }
         }
     });
