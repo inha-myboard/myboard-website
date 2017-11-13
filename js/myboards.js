9bcba08dcaf61f308f59da9a85c6d9e313c560b7
bdh92123 <bdh92123.1@gmail.com>
2017-11-14 04:16:28 +0900

    Implementing add widget wizard...

--- js/myboards.js
+++ js/myboards.js
@@ -26,19 +26,22 @@ function saveBoard() {
 }
 
 // API 관리창 로딩
-function onShowManageApi() {
-    $("#manageApiModal .nav a[href='#apiListPane']").tab("show");
+function onShowManageWidgetModal() {
     $.ajax({
-        "url": MYBOARD_HOST + "/users/api",
+        "url": MYBOARD_HOST + "/users/1/widgets",
         "success": function(data) {
-            var tableEl = templates["table-manage-api"](data);
-            $("#manageApiTable").html(tableEl);
+            var tableEl = templates["table-manage-widget"](data);
+            $("#manageWidgetTable").html(tableEl);
         }
     });
 }
 
-function onShowAddWidgetWizard() {
-
+function onShowAddWidgetModal() {
+    $("#addWidgetModal .nav > li:eq(0) a").tab("show");
+    $("#prevButton").hide();
+    $("#finishButton").addClass("hidden");
+    $("#nextButton").show();
+    $("#apiJsonText").val("");
 }
 
 // 위젯 생성
@@ -50,11 +53,34 @@ function makeWidget(widgetTemplate, data) {
     return widgetEl;
 }
 
-// 위젯 추가
+// 대시보드 위젯 추가
 function addWidget(widgetTemplate, data) {
     var gridstack = $('.grid-stack').data("gridstack");
     var widgetEl = makeWidget(widgetTemplate, data);
     gridstack.addWidget(widgetEl); // el, x, y, width, height, autoPosition, minWidth, maxWidth, minHeight, maxHeight, id]
+}
+
+// 위젯추가 스텝넘기기전 검사
+function checkStep(id) {
+    if(id == "step1") {
+        try {
+            JSON.parse($("#apiJsonText").val());
+        } catch(e) {
+            return "Please valid input json";
+        }
+    } else if(id == "step2") {
+        if(!addWidgetData.type) {
+            return "Please select widget type";
+        }
+    }
+}
+
+// 위젯추가 스텝넘기고나서
+function onShowStep(id) {
+    if(id == "step2") {
+        addWidgetData.apiJson = JSON.parse($("#apiJsonText").val());
+        addWidgetData.type = null;
+    }
 }
 
 // 이벤트 바인딩
@@ -69,17 +95,68 @@ function bindEvent() {
         saveBoard();
         disableEdit();
     });
-    $('#manageApiTable').on('click', '.clickable-row', function(event) {
+    $('#manageWidgetTable').on('click', '.clickable-row', function(event) {
         $(this).addClass('active').siblings().removeClass('active');
-        $("#manageApiTable").data("selectedRow", this);
+        $("#manageWidgetTable").data("selectedRow", this);
     });
 
-    $("#manageApiModal").on("show.bs.modal", function(){
-        onShowManageApi();
+    $("#addWidgetModal").on("show.bs.modal", function(){
+        onShowAddWidgetModal();
     });
+
+    $("#manageWidgetModal").on("show.bs.modal", function(){
+        onShowManageWidgetModal();
+    });
+
+    $("#nextButton").on("click", function(){
+        var curPane = $("#addWidgetModal .tab-content > .active");
+        var msg = checkStep(curPane.attr("id"));
+        if(msg) {
+            alert(msg);
+            return;
+        }
+        var nextPane = curPane.next();
+        if(nextPane.size() > 0) {
+            $("a[href='#" + nextPane.attr("id") + "']").tab("show");
+        }
+        if(nextPane.next().size() == 0) {
+            $("#finishButton").removeClass("hidden");
+            $("#nextButton").hide();
+        }
+        $("#prevButton").show();
+        onShowStep(nextPane.attr("id"));
+    });
+
+    $("#prevButton").on("click", function(){
+        var prevPane = $("#addWidgetModal .tab-content > .active").prev();
+        if(prevPane.size() > 0) {
+            $("a[href='#" + prevPane.attr("id") + "']").tab("show");
+        }
+        if(prevPane.prev().size() == 0) {
+            $("#prevButton").hide();
+        }
+        $("#finishButton").addClass("hidden");
+        $("#nextButton").show();
+        onShowStep(prevPane.attr("id"));
+    });
+
+    $("#finishButton").on("click", function(){
+        console.log(addWidgetData);
+        $("#addWidgetModal").modal("hide");
+        $("#manageWidgetModal").modal("show");
+    });
+
+    $("[data-type='widgetType'").on("click", function(){
+        addWidgetData.type = $(this).data("value");
+        $("#nextButton").click();
+    })
 }
 // Handlebars templates
 var templates = {};
+var addWidgetData = {
+    "type": "",
+    "apiJson": ""
+};
 
 $(document).on("ready", function(){
     $('.grid-stack').gridstack();
