34c1d8d583c229e0c722f9956310c98d57a407ab
bdh92123 <bdh92123.1@gmail.com>
2017-11-14 18:00:25 +0900

    Demo

---
+++ 지빠귀/js/extends.js
@@ -0,0 +1,316 @@
+jQuery.fn.extend({
+    getPath: function(root) {
+        var pathes = [];
+        if($(root)[0] === this[0]) return "";
+        this.each(function(index, element) {
+            var path, $node = jQuery(element);
+
+            while ($node.length) {
+                var realNode = $node.get(0), name = realNode.localName;
+                if (!name) { break; }
+
+                name = name.toLowerCase();
+                var parent = $node.parent();
+                var sameTagSiblings = parent.children(name);
+
+                if (sameTagSiblings.length > 1)
+                {
+                    allSiblings = parent.children();
+                    var index = allSiblings.index(realNode) +1;
+                    if (index > 0) {
+                        name += ':nth-child(' + index + ')';
+                    }
+                }
+
+                path = name + (path ? ' > ' + path : '');
+                $node = parent;
+
+                if(root != undefined && $node[0] === $(root)[0]) {
+                    break;
+                }
+            }
+
+            pathes.push(path);
+        });
+
+        return pathes.join(',');
+    },
+
+    visibleText: function(trim, joinChar) {
+        return $.map(this.contents(), function(el) {
+            if (el.nodeType === 3) {
+                return trim ? $(el).text().trim() : $(el).text();
+            }
+            if ($(el).is(':visible')) {
+                return $(el).visibleText();
+            }
+        }).join(joinChar ? joinChar : "");
+    }
+});
+
+jQuery.expr[':'].hasTextOnly = function(el, i) {
+    if(el.childNodes.length === 1 && el.firstChild.nodeType === 3) {
+        return jQuery.trim(el.innerHTML).length > 0;
+    }
+    return false;
+};
+
+jQuery.expr[':'].hasText = function(el, i) {
+    for(var i=0; i<el.childNodes.length; i++) {
+        if(el.childNodes[i].nodeType === 3 && jQuery.trim(el.childNodes[i].textContent).length > 0) {
+            return true;
+        }
+    }
+    
+    return false;
+};
+
+String.prototype.omit = function (maxlength) {
+    if(this.length > maxlength) {
+        return this.substring(0, maxlength) + "...";
+    }
+    return this;
+}
+
+
+// for demo: http://jsbin.com/jeqesisa/7/edit
+// for detailed comments, see my SO answer here http://stackoverflow.com/questions/8853396/logical-operator-in-a-handlebars-js-if-conditional/21915381#21915381
+
+/* a helper to execute an IF statement with any expression
+ USAGE:
+ -- Yes you NEED to properly escape the string literals, or just alternate single and double quotes
+ -- to access any global function or property you should use window.functionName() instead of just functionName()
+ -- this example assumes you passed this context to your handlebars template( {name: 'Sam', age: '20' } ), notice age is a string, just for so I can demo parseInt later
+ <p>
+ {{#xif " this.name == 'Sam' && this.age === '12' " }}
+ BOOM
+ {{else}}
+ BAMM
+ {{/xif}}
+ </p>
+ */
+Handlebars.registerHelper('include', function (path, context, options) {
+  var partial = Handlebars.partials[path];
+  if (typeof partial !== 'function') {
+    partial = Handlebars.compile(partial);
+  }
+  return partial(context); // build up the context some how
+});
+
+Handlebars.registerHelper('ifnull', function (value, safeValue) {
+    var out = value || safeValue;
+    return new Handlebars.SafeString(out);
+});
+
+Handlebars.registerHelper('join', function (joinChar) {
+    var out = Array.prototype.slice.call(arguments, 1, arguments.length - 1).join(joinChar);
+    return new Handlebars.SafeString(out);
+});
+
+Handlebars.registerHelper("omit", function(text, maxlength) {
+    return text.omit(maxlength);
+});
+
+Handlebars.registerHelper("xif", function (expression, options) {
+    return Handlebars.helpers["x"].apply(this, [expression, options]) ? options.fn(this) : options.inverse(this);
+});
+
+/* a helper to execute javascript expressions
+ USAGE:
+ -- Yes you NEED to properly escape the string literals or just alternate single and double quotes
+ -- to access any global function or property you should use window.functionName() instead of just functionName(), notice how I had to use window.parseInt() instead of parseInt()
+ -- this example assumes you passed this context to your handlebars template( {name: 'Sam', age: '20' } )
+ <p>Url: {{x " \"hi\" + this.name + \", \" + window.location.href + \" <---- this is your href,\" + " your Age is:" + window.parseInt(this.age, 10) "}}</p>
+ OUTPUT:
+ <p>Url: hi Sam, http://example.com <---- this is your href, your Age is: 20</p>
+ */
+
+Handlebars.registerHelper("x", function (expression, options) {
+    var fn = function(){}, result;
+    try {
+        fn = Function.apply(this, ["window", "return " + expression + " ;"]);
+    } catch (e) {
+        console.warn("{{x " + expression + "}} has invalid javascript", e);
+    }
+
+    try {
+        result = fn.call(this, window);
+    } catch (e) {
+        console.warn("{{x " + expression + "}} hit a runtime error", e);
+    }
+    return result;
+});
+
+
+/*
+ if you want access upper level scope, this one is slightly different
+ the expression is the JOIN of all arguments
+ usage: say context data looks like this:
+
+ // data
+ {name: 'Sam', age: '20', address: { city: 'yomomaz' } }
+
+ // in template
+ // notice how the expression wrap all the string with quotes, and even the variables
+ // as they will become strings by the time they hit the helper
+ // play with it, you will immediately see the errored expressions and figure it out
+
+ {{#with address}}
+ {{z '"hi " + "' ../this.name '" + " you live with " + "' city '"' }}
+ {{/with}}
+ */
+Handlebars.registerHelper("z", function () {
+    var options = arguments[arguments.length - 1]
+    delete arguments[arguments.length - 1];
+    return Handlebars.helpers["x"].apply(this, [Array.prototype.slice.call(arguments, 0).join(''), options]);
+});
+
+Handlebars.registerHelper("zif", function () {
+    var options = arguments[arguments.length - 1]
+    delete arguments[arguments.length - 1];
+    return Handlebars.helpers["x"].apply(this, [Array.prototype.slice.call(arguments, 0).join(''), options]) ? options.fn(this) : options.inverse(this);
+});
+
+
+
+/*
+ More goodies since you're reading this gist.
+ */
+
+// say you have some utility object with helpful functions which you want to use inside of your handlebars templates
+
+util = {
+
+    // a helper to safely access object properties, think ot as a lite xpath accessor
+    // usage:
+    // var greeting = util.prop( { a: { b: { c: { d: 'hi'} } } }, 'a.b.c.d');
+    // greeting -> 'hi'
+
+    // [IMPORTANT] THIS .prop function is REQUIRED if you want to use the handlebars helpers below,
+    // if you decide to move it somewhere else, update the helpers below accordingly
+    prop: function() {
+        if (typeof props == 'string') {
+            props = props.split('.');
+        }
+        if (!props || !props.length) {
+            return obj;
+        }
+        if (!obj || !Object.prototype.hasOwnProperty.call(obj, props[0])) {
+            return null;
+        } else {
+            var newObj = obj[props[0]];
+            props.shift();
+            return util.prop(newObj, props);
+        }
+    },
+
+    // some more helpers .. just examples, none is required
+    isNumber: function(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    },
+    daysInMonth: function (m, y) {
+        y = y || (new Date).getFullYear();
+        return /8|3|5|10/.test(m) ? 30 : m == 1 ? (!(y % 4) && y % 100) || !(y % 400) ? 29 : 28 : 31;
+    },
+    uppercaseFirstLetter: function (str) {
+        str || (str = '');
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+    hasNumber: function (n) {
+        return !isNaN(parseFloat(n));
+    },
+    truncate: function (str, len) {
+        if (typeof str != 'string') return str;
+        len = util.isNumber(len) ? len : 20;
+        return str.length <= len ? str : str.substr(0, len - 3) + '...';
+    }
+};
+
+// a helper to execute any util functions and get its return
+// usage: {{u 'truncate' this.title 30}} to truncate the title
+Handlebars.registerHelper('u', function() {
+    var key = '';
+    var args = Array.prototype.slice.call(arguments, 0);
+
+    if (args.length) {
+        key = args[0];
+        // delete the util[functionName] as the first element in the array
+        args.shift();
+        // delete the options arguments passed by handlebars, which is the last argument
+        args.pop();
+    }
+    if (util.hasOwnProperty(key)) {
+        // notice the reference to util here
+        return typeof util[key] == 'function' ?
+            util[key].apply(util, args) :
+            util[key];
+    } else {
+        log.error('util.' + key + ' is not a function nor a property');
+    }
+});
+
+// a helper to execute any util function as an if helper,
+// that util function should have a boolean return if you want to use this properly
+// usage: {{uif 'isNumber' this.age}} {{this.age}} {{else}} this.dob {{/uif}}
+Handlebars.registerHelper('uif', function() {
+    var options = arguments[arguments.length - 1];
+    return Handlebars.helpers['u'].apply(this, arguments) ? options.fn(this) : options.inverse(this);
+});
+
+// a helper to execute any global function or get global.property
+// say you have some globally accessible metadata i.e
+// window.meta = {account: {state: 'MA', foo: function() { .. }, isBar: function() {...} } }
+// usage:
+// {{g 'meta.account.state'}} to print the state
+
+// or will execute a function
+// {{g 'meta.account.foo'}} to print whatever foo returns
+Handlebars.registerHelper('g', function() {
+    var path, value;
+    if (arguments.length) {
+        path = arguments[0];
+        delete arguments[0];
+
+        // delete the options arguments passed by handlebars
+        delete arguments[arguments.length - 1];
+    }
+
+    // notice the util.prop is required here
+    value = util.prop(window, path);
+    if (typeof value != 'undefined' && value !== null) {
+        return typeof value == 'function' ?
+            value.apply({}, arguments) :
+            value;
+    } else {
+        log.warn('window.' + path + ' is not a function nor a property');
+    }
+});
+
+// global if
+// usage:
+// {{gif 'meta.account.isBar'}} // to execute isBar() and behave based on its truthy or not return
+// or just check if a property is truthy or not
+// {{gif 'meta.account.state'}} State is valid ! {{/gif}}
+Handlebars.registerHelper('gif', function() {
+    var options = arguments[arguments.length - 1];
+    return Handlebars.helpers['g'].apply(this, arguments) ? options.fn(this) : options.inverse(this);
+});
+
+// just an {{#each}} warpper to iterate over a global array,
+// usage say you have: window.meta = { data: { countries: [ {name: 'US', code: 1}, {name: 'UK', code: '44'} ... ] } }
+// {{geach 'meta.data.countries'}} {{this.code}} {{/geach}}
+
+Handlebars.registerHelper('geach', function(path, options) {
+    var value = util.prop(window, arguments[0]);
+    if (!_.isArray(value))
+        value = [];
+    return Handlebars.helpers['each'].apply(this, [value, options]);
+});
+
+Handlebars.registerHelper( 'eachInMap', function ( map, block ) {
+    var out = '';
+    Object.keys( map ).map(function( prop ) {
+        out += block.fn( {key: prop, value: map[ prop ]} );
+    });
+    return out;
+} );
---
+++ 지빠귀/js/myboards.js
@@ -0,0 +1,336 @@
+// var MYBOARD_HOST = "http://myboards.ml";
+var MYBOARD_HOST = "http://local.myboards.ml";
+
+// 편집모드 활성화
+function enableEdit() {
+    var gridstack = $('.grid-stack').data("gridstack");
+    gridstack.movable(gridstack.container.children('.' + gridstack.opts.itemClass), true);
+    gridstack.resizable(gridstack.container.children('.' + gridstack.opts.itemClass), true);
+    $(".nav-edit", "ul.nav").show();
+    $(".nav-normal", "ul.nav").hide();
+    $(".btn-box-tool", "div.box-header").show();
+    $(".mfb-component__wrap", "ul.mfb-component--br").show();
+}
+
+// 편집모드 비활성화
+function disableEdit() {
+    var gridstack = $('.grid-stack').data("gridstack");
+    gridstack.disable();
+    $(".nav-edit", "ul.nav").hide();
+    $(".nav-normal", "ul.nav").show();
+    $(".btn-box-tool", "div.box-header").hide();
+    $(".mfb-component__wrap", "ul.mfb-component--br").hide();
+}
+
+// 편집중인 보드 전체저장
+function saveBoard() {
+
+}
+
+// 위젯 삭제
+function removeWidget() {
+
+}
+
+// API 관리창 로딩
+function onShowManageWidgetModal() {
+    $.ajax({
+        "url": MYBOARD_HOST + "/users/1/widgets",
+        "success": function(data) {
+            var tableEl = templates["table-manage-widget"](data);
+            $("#manageWidgetTable").html(tableEl);
+        }
+    });
+}
+
+function onShowAddWidgetModal() {
+    $("#addWidgetModal .nav > li:eq(0) a").tab("show");
+    $("#prevButton").hide();
+    $("#finishButton").addClass("hidden");
+    $("#nextButton").show();
+    $("#apiJsonText").val("");
+}
+
+// 위젯 생성
+function makeWidget(widgetTemplate, data) {
+    var widgetEl = templates["widget-wrapper"]({
+        "widget": widgetTemplate,
+        "data": data
+    });
+    return widgetEl;
+}
+
+// 대시보드 위젯 추가
+function addWidget(widgetTemplate, data) {
+    var gridstack = $('.grid-stack').data("gridstack");
+    var widgetEl = makeWidget(widgetTemplate, data);
+    gridstack.addWidget(widgetEl); // el, x, y, width, height, autoPosition, minWidth, maxWidth, minHeight, maxHeight, id]
+}
+
+// API JSON 검사
+function validApiJson() {
+    if(addWidgetData.apiJson) {
+        if(addWidgetData.apiJson["body_selector"] && addWidgetData.apiJson["segments"] && addWidgetData.apiJson["segments"] )
+            return true;
+    }
+
+    return false;
+}
+
+// API JSON 을 Widget JSON로 변환
+function convertApiToWidgetJson(apiJson, widgetType) {
+    var widgetJson = null;
+    if(widgetType == " single") {
+        widgetJson = {
+            mapping_json: {
+                headers: [],
+                fields: []
+            },
+            props_json: {
+                bound: {}
+            }
+        };
+        widgetJson.type = "single";
+        for(var i in apiJson.segments) {
+            var segment = apiJson.segments[i];
+            widgetJson.mapping_json.headers.push({
+                "caption": segment.name,
+                "width": ""
+            });
+            widgetJson.mapping_json.fields.push({
+                "api_path": segment.name,
+                "style": {
+                    "text-align":"left",
+                    "font-size":"12px",
+                    "color":"#000000",
+                    "background-color":"#ffffff"
+                }
+            });
+        }
+    } else if(widgetType.startsWith("composite")) {
+        widgetJson = {
+            mapping_json: {
+                fields_position: "",
+                main_field: {},
+                fields: []
+            },
+            props_json: {
+                bound: {}
+            }
+        };
+        widgetJson.type = "composite";
+        if(widgetType == "compositeA") {
+            wigetJson.mapping_json.fields_position= "right";
+        } else if(widgetType == "compositeB") {
+            wigetJson.mapping_json.fields_position= "left";
+        } else if(widgetType == "compositeC") {
+            wigetJson.mapping_json.fields_position= "down";
+        }
+        for(var i in apiJson.segments) {
+            var segment = apiJson.segments[i];
+            widgetJson.mapping_json.fields.push({
+                "api_path": segment.name,
+                "style": {
+                    "text-align":"left",
+                    "font-size":"12px",
+                    "color":"#000000",
+                    "background-color":"#ffffff"
+                }
+            });
+        }
+    }
+
+    return widgetJson;
+}
+
+// 위젯추가 스텝넘기기전 검사
+function checkStep(id) {
+    if(id == "step1") {
+        try {
+            JSON.parse($("#apiJsonText").val());
+        } catch(e) {
+            return "Please valid input json";
+        }
+    } else if(id == "step2") {
+        if(!addWidgetData.type) {
+            return "Please select widget type";
+        }
+        if(!validApiJson()) {
+            return "API Json is incorrect. Please check 'body_selector', 'segments', 'url' fields. ";
+        }
+    } else if(id == "step3") {
+
+    }
+}
+
+// 위젯추가 스텝넘기고나서
+function onShowStep(id) {
+    if(id == "step2") {
+        addWidgetData.apiJson = JSON.parse($("#apiJsonText").val());
+        addWidgetData.type = null;
+    } else if(id == "step3") {
+        var widgetJson = convertApiToWidgetJson(addWidgetData.apiJson, addWidgetData.type);
+        var widgetMappingEl = templates["widget-mapping-" + widgetJson.type](widgetJson);
+        var segmentsMappingEl = templates["segments-mapping"](addWidgetData.apiJson);
+        $("#mappingTable").html(widgetMappingEl);
+        $("#mappingSegments").html(segmentsMappingEl);
+    }
+}
+
+// 이벤트 바인딩
+function bindEvent() {
+    $("#editButton").on("click", function(){
+        enableEdit();   
+    });
+    $("#exitButton").on("click", function(){
+        disableEdit();
+    });
+    $("#saveButton").on("click", function(){
+        saveBoard();
+        disableEdit();
+    });
+    $('#manageWidgetTable').on('click', '.clickable-row', function(event) {
+        $(this).addClass('active').siblings().removeClass('active');
+        $("#manageWidgetTable").data("selectedRow", this);
+    });
+
+    $("#addWidgetModal").on("show.bs.modal", function(){
+        onShowAddWidgetModal();
+    });
+
+    $("#manageWidgetModal").on("show.bs.modal", function(){
+        onShowManageWidgetModal();
+    });
+
+    $("#nextButton").on("click", function(){
+        var curPane = $("#addWidgetModal .tab-content > .active");
+        var msg = checkStep(curPane.attr("id"));
+        if(msg) {
+            alert(msg);
+            return;
+        }
+        var nextPane = curPane.next();
+        if(nextPane.size() > 0) {
+            $("a[href='#" + nextPane.attr("id") + "']").tab("show");
+        }
+        if(nextPane.next().size() == 0) {
+            $("#finishButton").removeClass("hidden");
+            $("#nextButton").hide();
+        }
+        $("#prevButton").show();
+        onShowStep(nextPane.attr("id"));
+    });
+
+    $("#prevButton").on("click", function(){
+        var prevPane = $("#addWidgetModal .tab-content > .active").prev();
+        if(prevPane.size() > 0) {
+            $("a[href='#" + prevPane.attr("id") + "']").tab("show");
+        }
+        if(prevPane.prev().size() == 0) {
+            $("#prevButton").hide();
+        }
+        $("#finishButton").addClass("hidden");
+        $("#nextButton").show();
+        onShowStep(prevPane.attr("id"));
+    });
+
+    $("#finishButton").on("click", function(){
+        console.log(addWidgetData);
+        $("#addWidgetModal").modal("hide");
+        $("#manageWidgetModal").modal("show");
+    });
+
+    $("[data-type='widgetType'").on("click", function(){
+        addWidgetData.type = $(this).data("value");
+        $("#nextButton").click();
+    })
+}
+// Handlebars templates
+var templates = {};
+
+// Temporary Widget json
+var addWidgetData = {
+    "type": "",
+    "apiJson": {},
+    "widgetJson": {}
+};
+
+$(document).on("ready", function(){
+    // var sse = new EventSource(url);
+    // sse.onmessage = function(message){
+    //     var data = message.data;
+    //     var widgets = message.widgets;
+
+    //     widgets[0] = $.extend(true, {}, widgets[0], {
+    //         // additional props
+    //         "props_json": {
+    //             "bound": {
+    //                 "x": 0,
+    //                 "y": 0,
+    //                 "width": 9,
+    //                 "height": 4
+    //             },
+    //             "border-top-color": "#00a65a"
+    //         }
+    //     })
+    //     widgets[1] = $.extend(true, {}, widgets[1], {
+    //         // additional props
+    //         "props_json": {
+    //             "bound": {
+    //                 "x": 0,
+    //                 "y": 0,
+    //                 "width": 9,
+    //                 "height": 4
+    //             },
+    //             "border-top-color": "#00a65a"
+    //         }
+    //     })
+    //     widgets[2] = $.extend(true, {}, widgets[2], {
+    //         // additional props
+    //         "props_json": {
+    //             "bound": {
+    //                 "x": 0,
+    //                 "y": 0,
+    //                 "width": 9,
+    //                 "height": 4
+    //             },
+    //             "border-top-color": "#00a65a"
+    //         }
+    //     })
+    // }
+    // sse.close();
+    
+    $('.grid-stack').gridstack();
+    var gridstack = $('.grid-stack').data("gridstack");
+    $.when(
+      $.ajax({
+          "url": "html/templates.html",
+          "dataType": "html"
+      })).done(function(data){
+            var html = $(data);
+            html.filter("script").each(function(i, script) {
+                if(script.type=="text/x-handlebars-tpl") {
+                    templates[script.id] = Handlebars.compile(script.innerHTML);
+                } else if(script.type=="text/x-handlebars-ptl") {
+                    Handlebars.registerPartial(script.id, script.innerHTML);
+                }
+            });
+
+            // Test Widget Script
+            testData[3] = testData[2];
+            testData[4] = testData[2];
+            for(var i = 0 ; i < testWidgets.length; i++) {
+                addWidget(testWidgets[i], testData[i]);    
+            }
+
+            
+
+    });
+
+      // $("#manageApiModal .modal-body .nav").hide();
+    
+    bindEvent();
+    disableEdit();
+
+    // TODO - next/prev button
+});
